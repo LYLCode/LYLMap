6c9db96d7680b917bad2dedfef4993172ff17f70
itheima <itheima@luyuanlideimac.local>
2016-03-20 20:32:26 +0800

    初始化项目

---
+++ 指南针/指南针/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  指南针
+//
+//  Created by itheima on 16/3/20.
+//  Copyright © 2016年 卢远立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 指南针/指南针/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  指南针
+//
+//  Created by itheima on 16/3/20.
+//  Copyright © 2016年 卢远立. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
